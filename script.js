4579fcb3f1b497e03ce96a758f413732cd7cb50c
Sruthyjmallya <sruthyjmallya2018@gmail.com>
2021-05-08 11:51:02 +0530

    Add View more button

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     $(".nav-link").click(function(event) {
       $(".navbar-collapse").collapse('hide');
     });
+    $("#view-more").click(function(event){
+        var check = $(".view").hasClass("d-none");
+        if(check){
+            $(".view").removeClass("d-none");
+            $("#view-more").html("View Less");
+        }
+        else{
+            $(".view").addClass("d-none");
+            $("#view-more").html("View more");
+        }
+    });
   });
